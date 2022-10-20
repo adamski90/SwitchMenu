88fd6bd93bf47dadf2b49b077fad5be46f17875c
adamski90 <adam.fagan2@gmail.com>
2022-10-20 11:08:24 +0000

    name added

--- SwitchOperatedTextMenu.java
+++ SwitchOperatedTextMenu.java
@@ -16,7 +16,7 @@ public class SwitchOperatedTextMenu {
 			switch (menuItem) {
 			case 1:
 				System.out.println("You've chosen item #1");
-				// do something...
+				System.out.println("My name is Adam Fagan");
 				break;
 			case 2:
 				System.out.println("You've chosen item #2");
